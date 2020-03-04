bbd8966d94c204388b2fb5527cb3abca3df8412f
Quanh Do <Quanh.Do@slalom.com>
2020-03-03 23:38:38 -0800

    initial commit

---
+++ examples/simple-three-tier/aws-example/environments/stage/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-up-and-running-state"
+    key            = "test/terraform.tfstate"
+    region         = "us-west-2"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
